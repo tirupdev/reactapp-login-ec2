82e8a7f7f77a08639735574ddeef63b996f00b05
tirupdev <ulli.tirupataiah@gmailcom>
2025-07-03 06:07:48 +0530

    reactapp-ec2-nginx

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Login from './Login';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h2>React Login Page</h2>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
